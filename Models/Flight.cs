f088127a61bbf84f5d1cfd849ab51c7b96b44d46
Ryan Felix <therjfelix@gmail.com>
2017-10-25 21:51:52 -0700

    update Flight model

--- Models/Flight.cs
+++ Models/Flight.cs
@@ -11,8 +11,8 @@ namespace alaska_airlines_test.Models
     public string From { get; set; }
     public string To { get; set; }
     public int FlightNumber { get; set; }
-    public string Departs { get; set; }
-    public string Arrives { get; set; }
+    public DateTime Departs { get; set; }
+    public DateTime Arrives { get; set; }
     public int MainCabinPrice { get; set; }
     public int FirstClassPrice { get; set; }
   }
